672e0eddac618d90528f8c5f441a0e0ba52fe9a7
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-03-02 00:01:42 +0100

    Refactore code

---
+++ src/test/java/ma/enset/jpa/userroles/UserRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.jpa.userroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRolesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
